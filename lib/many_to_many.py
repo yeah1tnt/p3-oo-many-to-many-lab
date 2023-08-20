ce86372cf279b4ebfa3c4ea76b64a70bb1364460
Tong Tang <pichip22@hotmail.com>
2023-08-20 15:51:21 -0400

    completed

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,53 @@
 class Author:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+    
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+    
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+
+    def total_royalties(self):
+        return sum([contract.royalties for contract in self.contracts()])
 
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
 
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
 
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
+    
 class Contract:
-    pass
+    all = []
+    
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise TypeError("Invalid author")
+        else:
+            self.author = author
+        if not isinstance(book, Book):
+            raise TypeError("Invalid book")
+        else: 
+            self.book = book
+        if not isinstance(date, str):
+            raise TypeError("Invalid date")
+        else:
+            self.date = date
+        if not isinstance(royalties, int):
+            raise TypeError("Invalid royalties")
+        else:
+            self.royalties = royalties
+
+        Contract.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls):
+        return sorted(cls.all, key=lambda contract: contract.date)
